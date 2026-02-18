0227d4eb42262a3859bb453c3056d65efacc69eb
CVBGOD <mercwar01@gmail.com>
2026-02-18 03:04:24 -0500

    Create fire-gem.sh

---
+++ cjs/fire-gem.sh
@@ -0,0 +1,14 @@
+{
+  "AVIS_DISPATCH_REGISTRY": {
+    "VERSION": "5.4",
+    "STUBS": [
+      { "ID": "STUB_MOD", "ACTION": "INTERNAL_CHMOD", "TARGET": "fire-term_bin" },
+      { "ID": "STUB_COMPILE", "ACTION": "INTERNAL_NASM", "SOURCE": "fire-term.asm" },
+      { "ID": "STUB_RUN", "ACTION": "INTERNAL_EXEC", "BINARY": "./fire-term_bin" }
+    ],
+    "POST_DISPATCH": {
+      "LOGGER": "./fire-log_bin",
+      "FINALIZER": "./fire-end_bin"
+    }
+  }
+}
