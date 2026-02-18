ea9c0146606562f5026dc2c96fc0357402c177e7
CVBGOD <mercwar01@gmail.com>
2026-02-18 05:44:55 -0500

    Update fire-log.asm

--- fire-log.asm
+++ fire-log.asm
@@ -1,15 +1,40 @@
 section .data
-    sh_bin      db "/bin/bash", 0
-    sh_script   db "./fire-log.sh", 0
-    sh_arg      db "ASM_PULSE_COMPLETE", 0
-    
-    ; argv array for execve: ["/bin/bash", "./fire-log.sh", "ASM_PULSE_COMPLETE", NULL]
-    shell_argv  dq sh_bin, sh_script, sh_arg, 0
+    filename db "fire-asm.log", 0
+    msg      db "FIRE-PORTAL DISPATCH LOOPING", 10
+    msglen   equ $ - msg
 
 section .text
-    ; ... (fork logic) ...
-    mov rdi, sh_bin
-    lea rsi, [rel shell_argv]
-    xor rdx, rdx
-    mov rax, 59         ; sys_execve
+    global _start
+
+_start:
+    mov r15, 5              ; Set loop counter (5 iterations)
+
+.loop:
+    ; sys_open (rax=2), O_WRONLY|O_CREAT|O_APPEND (0x441)
+    mov rax, 2
+    mov rdi, filename
+    mov rsi, 1089           ; 0x441
+    mov rdx, 0644o
+    syscall
+
+    push rax                ; Save file descriptor
+    mov rdi, rax            ; Move fd to rdi for write
+    
+    ; sys_write (rax=1)
+    mov rax, 1
+    mov rsi, msg
+    mov rdx, msglen
+    syscall
+
+    ; sys_close (rax=3)
+    mov rax, 3
+    pop rdi                 ; Retrieve fd
+    syscall
+
+    dec r15                 ; Decrement counter
+    jnz .loop               ; Jump if not zero
+
+    ; sys_exit (rax=60)
+    mov rax, 60
+    xor rdi, rdi
     syscall
