b4eceef713bf313c2db966b6461529dfac35fc8b
CVBGOD <mercwar01@gmail.com>
2026-02-18 02:40:08 -0500

    Update fire-log.asm

--- fire-log.asm
+++ fire-log.asm
@@ -1,30 +1,53 @@
-; fire-log.asm - Kernel-level Log Injector
+; fire-log.asm - Multi-File Pulse Sync
 section .data
-    file_name db "fire-portal.log", 0
-    entry_msg db "[PULSE] Kernel Function Executed | Geo: Hardcoded", 10
-    len_entry equ $-entry_msg
+    target1 db "fire-portal.log", 0
+    target2 db "avis-beacon.json", 0
+    target3 db "cjs/geo-audit.log", 0
+    pulse   db "[PULSE] Multi-Target Log Sync Complete", 10
 
 section .text
     global _start
 
 _start:
-    ; Open for Appending
+    ; Iterate through targets 1-3
+    mov r15, 1
+.loop:
+    cmp r15, 1
+    je .t1
+    cmp r15, 2
+    je .t2
+    cmp r15, 3
+    je .t3
+    jmp .done
+
+.t1: lea rdi, [rel target1]
+     jmp .write
+.t2: lea rdi, [rel target2]
+     jmp .write
+.t3: lea rdi, [rel target3]
+     jmp .write
+
+.write:
     mov rax, 2          ; sys_open
-    mov rdi, file_name
-    mov rsi, 0101o      ; O_WRONLY | O_APPEND
+    mov rsi, 02101o     ; O_WRONLY | O_APPEND | O_CREAT
     mov rdx, 0644o
     syscall
-
-    ; Write Entry
-    mov rdi, rax        ; fd from open
+    mov rbx, rax        ; save fd
+    
     mov rax, 1          ; sys_write
-    mov rsi, entry_msg
-    mov rdx, len_entry
+    mov rdi, rbx
+    mov rsi, pulse
+    mov rdx, 41
     syscall
-
-    ; Close & Exit
+    
     mov rax, 3          ; sys_close
+    mov rdi, rbx
     syscall
+    
+    inc r15
+    jmp .loop
+
+.done:
     mov rax, 60
     xor rdi, rdi
     syscall
