2064174b3d179e20cc389c03ba9df5798759841d
CVBGOD <mercwar01@gmail.com>
2026-02-18 03:34:49 -0500

    Update fire-log.asm

--- fire-log.asm
+++ fire-log.asm
@@ -1,39 +1,15 @@
-; fire-log.asm - Multi-Target Append
 section .data
-    t1      db "fire-portal.log", 0
-    t2      db "avis-beacon.json", 0
-    msg     db '{"pulse": "DISPATCH_COMPLETE", "geo": "LOCKED"}', 10
-    len_m   equ $-msg
+    sh_bin      db "/bin/bash", 0
+    sh_script   db "./fire-log.sh", 0
+    sh_arg      db "ASM_PULSE_COMPLETE", 0
+    
+    ; argv array for execve: ["/bin/bash", "./fire-log.sh", "ASM_PULSE_COMPLETE", NULL]
+    shell_argv  dq sh_bin, sh_script, sh_arg, 0
 
 section .text
-    global _start
-
-_start:
-    ; Target 1: Portal Log
-    lea rdi, [rel t1]
-    call append_to_file
-
-    ; Target 2: Beacon Status
-    lea rdi, [rel t2]
-    call append_to_file
-
-    mov rax, 60
-    xor rdi, rdi
+    ; ... (fork logic) ...
+    mov rdi, sh_bin
+    lea rsi, [rel shell_argv]
+    xor rdx, rdx
+    mov rax, 59         ; sys_execve
     syscall
-
-append_to_file:
-    ; sys_open (O_WRONLY|O_APPEND|O_CREAT)
-    mov rax, 2
-    mov rsi, 02101o     ; Flags
-    mov rdx, 0644o      ; Mode
-    syscall
-    
-    mov rdi, rax        ; fd
-    mov rax, 1          ; sys_write
-    mov rsi, msg
-    mov rdx, len_m
-    syscall
-    
-    mov rax, 3          ; sys_close
-    syscall
-    ret
